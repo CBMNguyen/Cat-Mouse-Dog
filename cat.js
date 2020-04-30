b074f887c95f8b9d898ac22ff6ec028353684ac5
HieuNguyen <hieub1809236@student.ctu.edu.vn>
2020-04-30 15:26:36 +0700

    Add method eat for Cat and Dog

--- cat.js
+++ cat.js
@@ -1,3 +1,10 @@
 function Cat(){
-	
+	name =  "Cat";
+	age =  1;
+	weight = 1;
+	color = "orange";
+	stomach = [];
 }
+Cat.prototype.eat = function(mouse) {
+	this.stomach.push(mouse);
+};
--- mouse.js
+++ mouse.js
@@ -1,3 +1,10 @@
 function Mouse(){
-	
+	name =  "Cat";
+	age =  1;
+	weight = 1;
+	color = "orange";
+	stomach = [];
 }
+Mouse.prototype.eat = function(cat) {
+	this.stomach.push(cat);
+};
